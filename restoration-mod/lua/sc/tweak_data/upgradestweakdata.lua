19f1cfb32aaf8f99eaf0395c5d80ccaef76a3f35
Kfcdragon756 <2504809701@qq.com>
2025-07-07 09:21:33 +0800

    恢复意外修改的ECM RANGE

--- restoration-mod/lua/sc/tweak_data/upgradestweakdata.lua
+++ restoration-mod/lua/sc/tweak_data/upgradestweakdata.lua
@@ -686,7 +686,7 @@ Hooks:PostHook(UpgradesTweakData, "_init_pd2_values", "ResSkillsInit", function(
 	--ECMs: They're ECMs
 	self.ecm_jammer_base_battery_life = 10
 	self.ecm_jammer_base_low_battery_life = 4
-	self.ecm_jammer_base_range = 2400
+	self.ecm_jammer_base_range = 2500 --不要再动它了
 	self.ecm_feedback_min_duration = 10
 	self.ecm_feedback_max_duration = 10
 	self.ecm_feedback_interval = 1.2
